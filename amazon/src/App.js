5d0ddb4a818bed5687662ce1dafd9713bb6b036d
Pranay Kumar <pranaykumar31may1996@gmail.com>
2021-01-05 20:45:32 +0530

    First commit - Added all the required dependencies

---
+++ amazon/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      Amazon clone
+    </div>
+  );
+}
+
+export default App;
