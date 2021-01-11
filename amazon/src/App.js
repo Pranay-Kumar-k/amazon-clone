ffcde9bce2e28e9dad0165d2ece787ea0d3d6329
Pranay Kumar <pranaykumar31may1996@gmail.com>
2021-01-11 17:02:03 +0530

    Built the banner

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -5,6 +5,7 @@ import {
   Route
 } from "react-router-dom";
 import Header from "./components/Header"
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
@@ -19,7 +21,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header />
-            <h1>Homepage</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
